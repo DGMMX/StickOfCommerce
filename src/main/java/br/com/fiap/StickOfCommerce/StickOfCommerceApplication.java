65b27f4f698a7e8f61b73e3ded5642f570d90604
DGMMX <dgmmxs2@outlook.com>
2025-04-15 11:34:23 -0300

    Criando o esqueleto do codigo

---
+++ src/main/java/br/com/fiap/StickOfCommerce/StickOfCommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.StickOfCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickOfCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickOfCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/StickOfCommerce/StickOfCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.StickOfCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickOfCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
